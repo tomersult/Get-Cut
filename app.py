7bb193574ec487947d293f612c4233b0664e5d8e
Oriel <oriel.a@phytech.com>
2021-09-23 13:45:18 +0300

    addition

--- app.py
+++ app.py
@@ -2,20 +2,18 @@ from threading import Thread
 
 from flask import Flask
 from database import db
-from requests.appointment import appointment_bp
-from requests.barber_images import barber_images_bp
-from requests.notification import notification_bp, check_every_user_notification, auto_func_for_notification
-from requests.notification_counter import notification_counter_bp
-from requests.user import user_bp
-from requests.barber import barber_bp
-from requests.favorites import favorite_bp
-from requests.barber_information import barber_information_bp
-from requests.barber_haircut_types import barber_haircut_bp
-from requests.dayBook import daybook_bp
-from requests.rating import rating_bp
-from requests.user_images import user_images_bp
-from flask_apscheduler import APScheduler
-from apscheduler.schedulers.background import BackgroundScheduler
+from request.appointment import appointment_bp
+from request.barber_images import barber_images_bp
+from request.notification import notification_bp, check_every_user_notification, auto_func_for_notification
+from request.notification_counter import notification_counter_bp
+from request.user import user_bp
+from request.barber import barber_bp
+from request.favorites import favorite_bp
+from request.barber_information import barber_information_bp
+from request.barber_haircut_types import barber_haircut_bp
+from request.dayBook import daybook_bp
+from request.rating import rating_bp
+from request.user_images import user_images_bp
 
 app = Flask(__name__)
 
@@ -25,11 +23,10 @@ def create_app():
     app.config['SECRET_KEY'] = 'this_is_secret'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:04030403@localhost/users_db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['BARBER_IMAGE_UPLOAD_PATH'] = '/Users/oriel/PycharmProjects/finalProject/barber_uploads'
-    app.config['USER_IMAGE_UPLOAD_PATH'] = '/Users/oriel/PycharmProjects/finalProject/user_uploads/'
-    app.config['BARBER_PROFILE_IMAGE_PATH'] = '/Users/oriel/PycharmProjects/finalProject/barber_profile_images/'
+    app.config['BARBER_IMAGE_UPLOAD_PATH'] = 'C:/Users/Oriel/PycharmProjects/finalProject/barber_uploads'
+    app.config['USER_IMAGE_UPLOAD_PATH'] = 'C:/Users/Oriel/PycharmProjects/finalProject/user_uploads/'
+    app.config['BARBER_PROFILE_IMAGE_PATH'] = 'C:/Users/Oriel/PycharmProjects/finalProject/barber_profile_images/'
     app.config['ALLOWED_FORMAT'] = ['PNG', 'JPG', 'JPEG', 'GIF']
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost/users_db'
     db.init_app(app)
     app.register_blueprint(user_bp, url_prefix='')
     app.register_blueprint(barber_bp, url_prefix='')
