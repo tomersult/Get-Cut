71a8f07036577295cc799471306e49a905c6b18d
tomer <tomersult@gmail.com>
2021-09-25 22:07:00 +0300

    user images

--- request/favorites.py
+++ request/favorites.py
@@ -18,16 +18,18 @@ class Favorite(db.Model):
 def add_favorite_follow(current_user):
     data = request.get_json()
     favorites = Favorite.query.all()
+    barber = Barber.query.filter_by(public_id=data['barber_public_id']).first()
     for favorite in favorites:
         if (favorite.user_public_id == current_user.public_id) & (favorite.barber_public_id == data['barber_public_id']):
             db.session.delete(favorite)
+            db.session.commit()
+            barber.followers -= 1
             db.session.commit()
             return jsonify({'message': 'Now you unfollow this barber !'})
     new_favorite = Favorite(user_public_id=current_user.public_id, barber_public_id=data['barber_public_id'])
     db.session.add(new_favorite)
     db.session.commit()
 
-    barber = Barber.query.filter_by(public_id=data['barber_public_id']).first()
     if not barber:
         db.session.delete(new_favorite)
         db.session.commit()
--- request/user_images.py
+++ request/user_images.py
@@ -32,24 +32,20 @@ def allowed_image(filename):
 @token_required
 def add_image(current_user):
     data = request.get_json()
-    file_name = current_user.public_id + '.jpeg'
     images = UserImages.query.all()
+    id_num = 0
     for one_image in images:
-        if one_image.user_public_id == current_user.public_id:
-            image = base64.b64decode(str(data['image']))
-            image_path = (current_app.config['USER_IMAGE_UPLOAD_PATH'] + file_name)
-            img = Image.open(io.BytesIO(image))
-            img.save(image_path, 'jpeg')
-            one_image.image_name = file_name
-            db.session.commit()
-            return jsonify({'message': 'Profile image has been changed!'})
+        id_num = one_image.id
+    if id_num != 0:
+        id_num += 1
+    file_name = '(' + str(id_num) + ')' + current_user.public_id + '.jpeg'
     if not allowed_image(file_name):
         return jsonify({'message': 'Image format not allowed'})
 
     image = base64.b64decode(str(data['image']))
     image_path = (current_app.config['USER_IMAGE_UPLOAD_PATH'] + file_name)
     img = Image.open(io.BytesIO(image))
-    img.save(image_path, 'jpeg')
+    img.save(image_path, 'png')
 
     new_user_image = UserImages(user_public_id=current_user.public_id, image_name=file_name,
                                 description=data['description'])
@@ -60,16 +56,17 @@ def add_image(current_user):
 
 @user_images_bp.route('/userImage', methods=['GET'])
 @token_required
-def get_user_profile_images(current_user):
-    image = UserImages.query.filter_by(user_public_id=current_user.public_id).first()
-    if not image:
+def get_all_user_images(current_user):
+    images = UserImages.query.filter_by(user_public_id=current_user.public_id).all()
+    if not images:
         return jsonify({'message': 'This user still not upload an image!'})
 
     output = []
-    with open(current_app.config['USER_IMAGE_UPLOAD_PATH'] + image.image_name, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    image_data = {}
-    image_data['image'] = str(encoded_string)
-    image_data['description'] = image.description
-    output.append(image_data)
+    for image in images:
+        with open(current_app.config['USER_IMAGE_UPLOAD_PATH'] + image.image_name, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+        image_data = {}
+        image_data['image'] = str(encoded_string)
+        image_data['description'] = image.description
+        output.append(image_data)
     return jsonify(output)
